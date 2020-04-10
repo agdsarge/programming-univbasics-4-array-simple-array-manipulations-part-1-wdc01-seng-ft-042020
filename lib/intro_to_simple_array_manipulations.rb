4c237916944f1193456dea4f20cc3c4d984a42db
Andrew Sargeant <agdsargeant@gmail.com>
2020-04-10 19:38:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,6 +3,16 @@ def using_push(array, string)
   return array
 end
 
+def using_unshift(array, string)
+  array.unshift(string)
+  return array
+end
 
+def using_pop(array)
+  return array.pop
+end
 
+def pop_with_args(array)
+  
 
+end
